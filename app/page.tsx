583e4c85d10733d8495af9ee5ac9553a5e02d40c
Rafael Bezerra <rafaelmontreuil@gmail.com>
2023-12-07 15:52:10 -0300

    first-commit

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { Button } from '@nextui-org/react'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+    <main>
       
     </main>
   )
