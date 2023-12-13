333eb6c1d325013254df8027918e5796708ddc20
Rafael Bezerra <rafaelmontreuil@gmail.com>
2023-12-13 11:14:42 -0300

    first-commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
 import Image from 'next/image'
-import { Button } from '@nextui-org/react'
+import Make_a_bet1 from './components/make_a_bet1'
+import Payment_bet from '../page2/payment_bet'
 
 export default function Home() {
   return (
     <main>
-      
+      <Make_a_bet1 /> 
     </main>
   )
 }
---
+++ page2/page.tsx
@@ -0,0 +1,10 @@
+import Image from 'next/image'
+import Payment_bet from './payment_bet'
+
+export default function Home() {
+  return (
+    <main>
+      <Payment_bet />
+    </main>
+  )
+}
